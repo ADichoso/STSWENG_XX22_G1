05edfde247143954f62b59483e97ae0423b07f2e
Lunarianerino <71534058+Lunarianerino@users.noreply.github.com>
2024-02-27 08:36:56 +0800

    Create profile.test.js

---
+++ __test__/profile.test.js
@@ -0,0 +1,5 @@
+const profileController = require('../controllers/profileController.js');
+
+const User = require('../models/userdb.js');
+const Admin = require('../models/admindb.js');
+const Driver = require('../models/driverdb.js');
