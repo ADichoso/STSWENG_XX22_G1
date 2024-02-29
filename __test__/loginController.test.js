15f81c2fa66f6d51e93be6f23e5e4f278ce6b990
austinatividad <austin_natividad@dlsu.edu.ph>
2024-02-29 22:42:31 +0800

    Added failed test cases for loginController driver features

--- __test__/loginController.test.js
+++ __test__/loginController.test.js
@@ -3,11 +3,18 @@ const db = require('../models/db.js');
 const bcrypt = require('bcrypt');
 const User = require('../models/userdb.js');
 const Admin = require('../models/admindb.js');
+const Driver = require('../models/driverdb.js');
 
 jest.mock('../models/db.js');
 jest.mock('bcrypt');
 jest.mock('../models/userdb.js');
 jest.mock('../models/admindb.js');
+jest.mock('../models/driverdb.js');
+
+
+/*
+*   Database call order: User -> Admin -> Driver
+*/
 
 describe('loginController', () => {
   describe('getLogin', () => {
@@ -21,92 +28,123 @@ describe('loginController', () => {
     });
 
     test('should redirect to user profile if user is found in User collection', async () => {
-      const req = { session: { idNumber: '12196290' } };
+      const req = { session: { id_number: '12196290' } };
       const res = { status: jest.fn().mockReturnThis(), redirect: jest.fn() };
       const find_one_mock = jest.spyOn(db, 'findOne').mockResolvedValueOnce(true);
 
       await loginController.getLogin(req, res);
 
-      expect(find_one_mock).toHaveBeenCalledWith(User, { idNumber: '12196290' }, { idNumber: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(User, { id_number: '12196290' }, { id_number: 1 });
       expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.redirect).toHaveBeenCalledWith('/Profile?idNumber=12196290');
+      expect(res.redirect).toHaveBeenCalledWith('/Profile?id_number=12196290');
     });
 
     test('should redirect to admin profile if user is found in Admin collection', async () => {
-      const req = { session: { idNumber: '123456789' } };
+      const req = { session: { id_number: '123456789' } };
       const res = { status: jest.fn().mockReturnThis(), redirect: jest.fn() };
-      const findOneMock = jest.spyOn(db, 'findOne').mockResolvedValueOnce(false).mockResolvedValueOnce(true);
+      const find_one_mock = jest.spyOn(db, 'findOne').mockResolvedValueOnce(false).mockResolvedValueOnce(true);
 
       await loginController.getLogin(req, res);
 
-      expect(findOneMock).toHaveBeenCalledWith(User, { idNumber: '123456789' }, { idNumber: 1 });
-      expect(findOneMock).toHaveBeenCalledWith(Admin, { idNumber: '123456789' }, { idNumber: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(User, { id_number: '123456789' }, { id_number: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(Admin, { id_number: '123456789' }, { id_number: 1 });
       expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.redirect).toHaveBeenCalledWith('/ProfileAdmin?idNumber=123456789');
+      expect(res.redirect).toHaveBeenCalledWith('/ProfileAdmin?id_number=123456789');
     });
 
-    test('should render login page with isValid false if neither user nor admin found', async () => {
-      const req = { session: { idNumber: '123456789' } };
-      const res = { render: jest.fn() };
-      const findOneMock = jest.spyOn(db, 'findOne').mockResolvedValue(false);
+    test('should redirect to driver profile if user is found in Driver collection', async () => {
+      const req = { session: { id_number: '123456789' } };
+      const res = { status: jest.fn().mockReturnThis(), redirect: jest.fn() };
+      const find_one_mock = jest.spyOn(db, 'findOne').mockResolvedValueOnce(false).mockResolvedValueOnce(false).mockResolvedValueOnce(true);
 
       await loginController.getLogin(req, res);
 
-      expect(findOneMock).toHaveBeenCalledWith(User, { idNumber: '123456789' }, { idNumber: 1 });
-      expect(findOneMock).toHaveBeenCalledWith(Admin, { idNumber: '123456789' }, { idNumber: 1 });
-      expect(res.render).toHaveBeenCalledWith('Login', { isValid: false });
+      expect(find_one_mock).toHaveBeenCalledWith(User, { id_number: '123456789' }, { id_number: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(Admin, { id_number: '123456789' }, { id_number: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(Driver, { id_number: '123456789' }, { id_number: 1 });
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.redirect).toHaveBeenCalledWith('/ProfileDriver?id_number=123456789');
+    });
+
+    test('should render login page with is_valid false if account not foudn', async () => {
+      const req = { session: { id_number: '123456789' } };
+      const res = { render: jest.fn() };
+      const find_one_mock = jest.spyOn(db, 'findOne').mockResolvedValue(false);
+
+      await loginController.getLogin(req, res);
+
+      expect(find_one_mock).toHaveBeenCalledWith(User, { id_number: '123456789' }, { id_number: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(Admin, { id_number: '123456789' }, { id_number: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(Driver, { id_number: '123456789' }, { id_number: 1 });
+      expect(res.render).toHaveBeenCalledWith('Login', { is_valid: false });
     });
   });
 
   describe('postLogin', () => {
-    test('should redirect to SecurityCheck page if first time login is successful', async () => {
-      const req = { body: { user_idNumber: '123456789', user_password: 'password' }, session: {} };
+    test('should redirect to SecurityCheck page if login is successful', async () => {
+      const req = { body: { user_id_number: '123456789', user_password: 'password' }, session: {} };
       const res = { redirect: jest.fn() };
-      const findOneMock = jest.spyOn(db, 'findOne').mockResolvedValueOnce({ idNumber: '123456789', password: 'hashed' });
+      const find_one_mock = jest.spyOn(db, 'findOne').mockResolvedValueOnce({ id_number: '123456789', password: 'hashed' });
       bcrypt.compare.mockResolvedValueOnce(true);
 
       await loginController.postLogin(req, res);
 
-      expect(findOneMock).toHaveBeenCalledWith(User, { idNumber: '123456789' }, { idNumber: 1, password: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(User, { id_number: '123456789' }, { id_number: 1, password: 1 });
       expect(bcrypt.compare).toHaveBeenCalledWith('password', 'hashed');
-      expect(req.session.idNumber).toBe('123456789');
-      expect(res.redirect).toHaveBeenCalledWith('/SecurityCheck?idNumber=123456789');
+      expect(req.session.id_number).toBe('123456789');
+      expect(res.redirect).toHaveBeenCalledWith('/SecurityCheck?id_number=123456789');
     });
 
     test('should redirect to SecurityCheck page if admin login is successful', async () => {
-      const req = { body: { user_idNumber: '123456789', user_password: 'password' }, session: {} };
+      const req = { body: { user_id_number: '123456789', user_password: 'password' }, session: {} };
       const res = { redirect: jest.fn() };
-      const findOneMock = jest.spyOn(db, 'findOne').mockResolvedValueOnce(false).mockResolvedValueOnce({ idNumber: '123456789', password: 'hashedPassword' });
+      const find_one_mock = jest.spyOn(db, 'findOne').mockResolvedValueOnce(false).mockResolvedValueOnce({ id_number: '123456789', password: 'hashedPassword' });
       bcrypt.compare.mockResolvedValueOnce(true);
 
       await loginController.postLogin(req, res);
 
-      expect(findOneMock).toHaveBeenCalledWith(User, { idNumber: '123456789' }, { idNumber: 1, password: 1 });
-      expect(findOneMock).toHaveBeenCalledWith(Admin, { idNumber: '123456789' }, { idNumber: 1, password: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(User, { id_number: '123456789' }, { id_number: 1, password: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(Admin, { id_number: '123456789' }, { id_number: 1, password: 1 });
       expect(bcrypt.compare).toHaveBeenCalledWith('password', 'hashed');
-      expect(req.session.idNumber).toBe('123456789');
-      expect(res.redirect).toHaveBeenCalledWith('/SecurityCheck?idNumber=123456789');
+      expect(req.session.id_number).toBe('123456789');
+      expect(res.redirect).toHaveBeenCalledWith('/SecurityCheck?id_number=123456789');
     });
 
-    test('should render login page with isValid false if login fails', async () => {
-      const req = { body: { user_idNumber: '123456789', user_password: 'password' }, session: {} };
-      const res = { render: jest.fn() };
-      const findOneMock = jest.spyOn(db, 'findOne').mockResolvedValueOnce(false);
+    test('should redirect to SecurityCheck page if driver login is successful', async () => {
+      const req = { body: { user_id_number: '123456789', user_password: 'password' }, session: {} };
+      const res = { redirect: jest.fn() };
+      const find_one_mock = jest.spyOn(db, 'findOne').mockResolvedValueOnce(false).mockResolvedValueOnce(false).mockResolvedValueOnce({ id_number: '123456789', password: 'hashedPassword' });
+      bcrypt.compare.mockResolvedValueOnce(true);
 
       await loginController.postLogin(req, res);
 
-      expect(findOneMock).toHaveBeenCalledWith(User, { idNumber: '123456789' }, { idNumber: 1, password: 1 });
-      expect(res.render).toHaveBeenCalledWith('Login', { isValid: false });
+      expect(find_one_mock).toHaveBeenCalledWith(User, { id_number: '123456789' }, { id_number: 1, password: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(Admin, { id_number: '123456789' }, { id_number: 1, password: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(Driver, { id_number: '123456789' }, { id_number: 1, password: 1 });
+      expect(bcrypt.compare).toHaveBeenCalledWith('password', 'hashed');
+      expect(req.session.id_number).toBe('123456789');
+      expect(res.redirect).toHaveBeenCalledWith('/SecurityCheck?id_number=123456789');
+    });
+
+    test('should render login page with is_valid false if login fails', async () => {
+      const req = { body: { user_id_number: '123456789', user_password: 'password' }, session: {} };
+      const res = { render: jest.fn() };
+      const find_one_mock = jest.spyOn(db, 'findOne').mockResolvedValueOnce(false);
+
+      await loginController.postLogin(req, res);
+
+      expect(find_one_mock).toHaveBeenCalledWith(User, { id_number: '123456789' }, { id_number: 1, password: 1 });
+      expect(res.render).toHaveBeenCalledWith('Login', { is_valid: false });
     });
 
     test('should handle error by sending 500 status', async () => {
-      const req = { body: { user_idNumber: '123456789', user_password: 'password' }, session: {} };
+      const req = { body: { user_id_number: '123456789', user_password: 'password' }, session: {} };
       const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
-      const findOneMock = jest.spyOn(db, 'findOne').mockRejectedValueOnce('Error');
+      const find_one_mock = jest.spyOn(db, 'findOne').mockRejectedValueOnce('Error');
 
       await loginController.postLogin(req, res);
 
-      expect(findOneMock).toHaveBeenCalledWith(User, { idNumber: '123456789' }, { idNumber: 1, password: 1 });
+      expect(find_one_mock).toHaveBeenCalledWith(User, { id_number: '123456789' }, { id_number: 1, password: 1 });
       expect(res.status).toHaveBeenCalledWith(500);
       expect(res.send).toHaveBeenCalledWith('Error');
     });
