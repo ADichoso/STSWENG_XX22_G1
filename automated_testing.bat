693941bdf257231ec626f29090d826c411beccad
ren <154307471+ren-malthalash@users.noreply.github.com>
2024-04-04 21:06:31 +0800

    Update automated_testing.bat

--- automated_testing.bat
+++ automated_testing.bat
@@ -5,7 +5,6 @@ pip install robotframework
 pip install robotframework-seleniumlibrary
 
 echo Running Test Scripts
-node test_accounts.js
 robot --outputdir ./__test__/output/ ./__test__/. 
 python3 -m robot --outputdir ./__test__/output/ ./__test__/. 
 py -m robot --outputdir ./__test__/output/ ./__test__/. 
